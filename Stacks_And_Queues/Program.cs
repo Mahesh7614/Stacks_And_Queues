bf6d8dac5a4b51401db3032a2911010717ac5e77
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-08 22:37:46 +0530

    Create Stack

---
+++ Stacks_And_Queues/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Stacks_And_Queues
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Stacks stack = new Stacks();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+        }
+    }
+}
